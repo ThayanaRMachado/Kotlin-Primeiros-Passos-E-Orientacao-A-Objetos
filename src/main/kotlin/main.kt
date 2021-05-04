c32da2c9ab01a64ca29793636c97c7672e7a0b8d
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-04 13:05:06 -0300

    1.11 - Adicionando as demais variáveis

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,13 @@
 fun main(){
     println("Bem vindo ao ByteBank!")
-    val titular = "Alex"
+   val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo +=200
 
     println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+
 }
