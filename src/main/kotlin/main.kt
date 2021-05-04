31de4e4cddc0da26da17d217c0f6335c97d9f74b
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-04 18:47:41 -0300

    4.2 - Criando métodos da classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
+import kotlin.reflect.jvm.internal.impl.resolve.constants.ULongValue
+
 fun main() {
-    println("Bem vindo ao Bytebank!")
+    println("Bem vindo ao Bytebank")
     val contaAlex = Conta()
     contaAlex.titular = "Alex"
     contaAlex.numero = 1000
@@ -10,14 +12,56 @@ fun main() {
     contaFran.numero = 1001
     contaFran.saldo = 300.0
 
-    /*println(contaFran.titular)
+    println(contaFran.titular)
     println(contaFran.numero)
     println(contaFran.saldo)
 
     println(contaAlex.titular)
     println(contaAlex.numero)
-    println(contaAlex.saldo)*/
+    println(contaAlex.saldo)
 
+    println("depositando na conta do Alex")
+    contaAlex.deposita(50.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("Sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
+
+    println("Sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("Saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
+
+    println("Saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+    fun deposita(valor: Double){
+        this.saldo += valor
+    }
+
+    fun saca(valor: Double){
+        if(saldo >= valor){
+            saldo -= valor
+        }
+    }
+}
+
+fun testaCopiasEReferencias(){
     val numeroX = 10
     var numeroY = numeroX
     numeroY++
@@ -31,34 +75,51 @@ fun main() {
     contaMaria.titular = "Maria"
     contaJoao.titular = "João"
 
-    println("titular da conta joao: ${contaJoao.titular}")
-    println("titular da conta maria: ${contaMaria.titular}")
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
 
     println(contaJoao)
     println(contaMaria)
 }
 
-    class Conta {
-        var titular = ""
-        var numero = 0
-        var saldo = 0.0
+fun testaLacos(){
+    var i = 0
+    while(i < 5){
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
 
-        fun testaLacos() {
-            var i = 0;
-            while (i < 5){
-                val titular: String = "Alex $i"
-                i++
-            }
-
-            for (i in 5 downTo 1){
-                val titular: String = "Alex $i"
-                val numeroConta: Int = 1000 + i
-                var saldo: Double = i + 10.0
-
-                println("titular $titular")
-                println("número da conta $numeroConta")
-                println("saldo conta $saldo")
-                println()
-            }
-        }
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
     }
+
+    for (i in 5 downTo 1) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
+}
