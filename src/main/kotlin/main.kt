176f887106fc2ea63841a050ae0456f6be70eaf6
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-05 11:03:41 -0300

    2.4 - Sobrescrevendo a bonificação

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,7 +10,7 @@ fun main() {
     println("nome ${alex.nome}")
     println("cpf ${alex.cpf}")
     println("salario ${alex.salario}")
-    println("bonificação ${alex.bonificacao()}")
+    println("bonificação ${alex.bonificacao}")
 
     val fran = Gerente(
         nome = "Fran",
@@ -22,7 +22,7 @@ fun main() {
     println("nome ${fran.nome}")
     println("cpf ${fran.cpf}")
     println("salário ${fran.salario}")
-    println("bonificação ${fran.bonificacao()}")
+    println("bonificação ${fran.bonificacao}")
 
     if (fran.autentica(1234)){
         println("Autenticou com sucesso!")
@@ -41,7 +41,7 @@ fun main() {
     println("nome ${gui.nome}")
     println("cpf ${gui.cpf}")
     println("salário ${gui.salario}")
-    println("bonificação ${gui.bonificacao()}")
+    println("bonificação ${gui.bonificacao}")
     println("plr ${gui.plr}")
 
     if (gui.autentica(4000)){
