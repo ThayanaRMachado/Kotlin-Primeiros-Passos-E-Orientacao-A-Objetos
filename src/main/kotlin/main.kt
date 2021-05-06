1eeb8f1d6e0ab9eff13a4a6c9f06a29a59bfb152
ThayanaRMachado <thayanarm@hotmail.com>
2021-05-05 21:34:25 -0300

    5.3 - Herdando mais de uma classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,3 @@
 fun main() {
-    val gerente = Gerente(
-        nome = "Alex",
-        cpf = "234.654.876-09",
-        salario = 1000.0,
-        senha = 1000
-    )
-
-    val diretora = Diretor(
-        nome = "Fran",
-        cpf = "123.123.123-12",
-        salario = 2000.0,
-        senha = 2000,
-        plr = 200.0
-    )
-
-    val  sistema = SistemaInterno()
-    sistema.entra(gerente, 1000)
-    sistema.entra(diretora, 2000)
+    testaAutenticacao()
 }
